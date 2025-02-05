737f96fa1e1457ea0cb11829c10bce40d54cf6ca
DeepakShinde123 <Deepakshinde123@proton.me>
2025-02-05 07:19:20 +0000

    add submit button

--- client/components/Inputs.jsx
+++ client/components/Inputs.jsx
@@ -1,40 +1,40 @@
-import { Input } from "@heroui/react";
-import { useState } from "react";
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Button, Input } from "@heroui/react";
 import { SendHorizontalIcon, UploadIcon } from "lucide-react";
 
 function Inputs() {
   const [input, setInput] = useState("");
+  const inputUpload = useRef(null);
 
-  function handleInputChange(event) {
-    setInput(event.target.value);
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  function handleSendClick() {
-    if (input.trim()) {
-      console.log(input);
-      setInput("");
-    }
-  }
+    console.log(input);
+
+    inputUpload.current.click();
+
+    setInput("");
+  };
 
   return (
-    <div className="absolute bottom-0 left-0 w-full sm:mb-5 flex gap-4 p-4">
+    <form
+      className="absolute bottom-0 left-0 w-full sm:mb-5 flex sm:gap-1"
+      onSubmit={handleSubmit}
+    >
       <Input
         type="text"
         label="Enter your message"
         value={input}
-        onChange={handleInputChange}
-        className="flex-1"
+        onChange={(e) => setInput(e.target.value)}
+        autoComplete="off"
       />
-      <input type="file" hidden />
-      <button
-        onClick={handleSendClick}
-        className="h-auto bg-green-500 p-4 rounded-xl flex items-center justify-center"
-        aria-label="Send Message"
-      >
-        {input ? <SendHorizontalIcon size={20} /> : <UploadIcon />}
-      </button>
-    </div>
+
+      <input type="file" name="file" ref={inputUpload} hidden />
+
+      <Button className="h-auto bg-blue-400" type="submit">
+        {input ? <SendHorizontalIcon /> : <UploadIcon />}
+      </Button>
+    </form>
   );
 }
 
