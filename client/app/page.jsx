7bb363290b97c85cdadade55ac1f43a77ec4026e
DeepakShinde123 <Deepakshinde123@proton.me>
2025-02-10 08:19:35 +0000

    store new messages to state

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 const socket = io(
-  "https://crispy-space-fishstick-69745jxr6xph5wqg-8000.app.github.dev/"
+  "https://refactored-rotary-phone-5g4x69w5vxrvh4p96-8000.app.github.dev"
 );
 
 export default function Home() {
@@ -21,10 +21,10 @@ export default function Home() {
     <HeroUIProvider>
       <div className="min-h-screen max-h-screen">
         {user ? (
-          <div className="bg-green-100 min-h-screen">
+          <div className="bg-violet-200 min-h-screen">
             <div className="container mx-auto relative min-h-screen p-4">
               <Messages />
-              <Inputs />
+              <Inputs socket={socket} id={socket.id} name={user} />
             </div>
           </div>
         ) : (
